084fa83d53bb398ddf87a6cdf20e02b5dde7a2d2
XG520 <513205323@qq.com>
2025-01-17 18:02:35 +0800

    添加卸载方法

--- __init__.py
+++ __init__.py
@@ -1,7 +1,6 @@
 import logging
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.helpers.entity_component import async_remove_platforms
 from .const import (
     DOMAIN,
     LOGGER_NAME,
@@ -21,9 +20,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     try:
-        unload_ok = await async_remove_platforms(hass, entry, PLATFORMS)
+        unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
         if unload_ok:
-            # 清理集成的数据
             hass.data[DOMAIN].pop(entry.entry_id, None)
         return unload_ok
     except Exception as e:
